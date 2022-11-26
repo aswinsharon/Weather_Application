771f243a22d6c1d069c98dc5fa14d79ef20783c9
Aswin Sharon <62883870+aswinsharon@users.noreply.github.com>
2022-11-26 21:35:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const constants ={
 
     openWeatherApi:{
         URL:"https://api.openweathermap.org/data/2.5/weather?q=",
-        KEY:"f5abbc979d500d5f7b135c58878071ed",
+        KEY:"",
         Unit:"&units=metric"
     }
 }
